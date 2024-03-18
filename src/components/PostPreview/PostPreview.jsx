18c5dc0d7ef5806ff8a6dee4b91719e60920bf58
Kyle <berlinerkyle@gmail.com>
2024-03-18 16:21:35 -0600

    Added the file structure for the post modals.

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import styles from "./PostPreview.module.css";
 
 export default function PostPreview({ information }) {
-	console.log(information);
+	const handleViewPost = () => {};
+
 	return (
 		<>
-			<div className={styles.post_preview_container}>
+			<div
+				onClick={() => handleViewPost()}
+				className={styles.post_preview_container}
+			>
 				<img className={styles.post_preview_image} />
 				<h2 className={styles.post_preview_title}>{information.title}</h2>
 			</div>
