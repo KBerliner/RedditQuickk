eb79bb94b07eb7b73f68f679bcb49f11ba5f4792
Kyle <berlinerkyle@gmail.com>
2024-03-18 15:07:14 -0600

    Removed all template styling. Imported the logo svg. Added routing with an example route.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <h2>Hello world!</h2>,
+	},
+	{
+		path: "/example",
+		element: <h1>Example</h1>,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router}>
+				<App />
+			</RouterProvider>
+		</Provider>
+	</React.StrictMode>
+);
